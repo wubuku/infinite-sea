c748722493fd5b6178a2d1348cf823f3578827d8
whilliy <return@126.com>
2024-08-05 19:17:47 +0800

    根据玩家的钱包地址获取已经采集的矿产数量和伐木数量。

---
+++ sui-java-service/suiinfinitesea-service-rest/src/main/java/org/dddml/suiinfinitesea/sui/contract/repository/SkillProcessEventExtendRepository.java
@@ -0,0 +1,23 @@
+package org.dddml.suiinfinitesea.sui.contract.repository;
+
+
+import org.dddml.suiinfinitesea.domain.skillprocess.AbstractSkillProcessEvent;
+import org.dddml.suiinfinitesea.domain.skillprocess.SkillProcessEventId;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface SkillProcessEventExtendRepository extends
+        JpaRepository<AbstractSkillProcessEvent.CreationProcessCompleted, SkillProcessEventId> {
+
+    /*
+    SELECT IFNULL(SUM(spv.dynamic_properties->'$.quantity'),0) AS quantity
+FROM skill_process_event spv
+WHERE spv.event_type='CreationProcessCompleted' AND sui_sender='sui_sender' AND spv.dynamic_properties->'$.itemId'=301
+     */
+    @Query(value = "SELECT IFNULL(sum(spv.dynamic_properties->'$.quantity'),0) AS quantity " +
+            "FROM skill_process_event spv " +
+            "WHERE spv.event_type='CreationProcessCompleted' AND sui_sender=:suiSender " +
+            "AND spv.dynamic_properties->'$.itemId'=:itemId", nativeQuery = true)
+    Integer getCreationQuantity(@Param("suiSender") String suiSender, @Param("itemId") Long itemId);
+}
---
+++ sui-java-service/suiinfinitesea-service-rest/src/main/java/org/dddml/suiinfinitesea/sui/contract/restful/resource/QuestResource.java
@@ -0,0 +1,41 @@
+package org.dddml.suiinfinitesea.sui.contract.restful.resource;
+
+import org.dddml.suiinfinitesea.sui.contract.repository.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RequestMapping(path = "quests", produces = MediaType.APPLICATION_JSON_VALUE)
+@RestController
+public class QuestResource {
+    @Autowired
+    private ItemEventRepository itemEventRepository;
+
+    @Autowired
+    private ShipProductionEventRepository shipProductionEventRepository;
+    @Autowired
+    private FaucetRequestedEventRepository faucetRequestedEventRepository;
+
+    @Autowired
+    private ShipBattleRelatedRepository shipBattleRelatedRepository;
+
+    @Autowired
+    private SkillProcessEventExtendRepository skillProcessEventExtendRepository;
+
+    @GetMapping(path = "minedOreQuantity")
+    @Transactional(readOnly = true)
+    public Integer getOreQuantity(@RequestParam(value = "senderAddress") String senderAddress) {
+        return skillProcessEventExtendRepository.getCreationQuantity(senderAddress, 301L);
+    }
+
+    @GetMapping(path = "cutWoodQuantity")
+    @Transactional(readOnly = true)
+    public Integer getWoodQuantity(@RequestParam(value = "senderAddress") String senderAddress) {
+        return skillProcessEventExtendRepository.getCreationQuantity(senderAddress, 200L);
+    }
+}
